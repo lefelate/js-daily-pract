9bd0941ba7b43e9b4066e8e99ff4c81da2464432
lefelate <44091564+lefelate@users.noreply.github.com>
2023-09-11 01:22:55 +0000

    javascript practice

---
+++ hidden_search_widget/script.js
@@ -0,0 +1,8 @@
+const searchButton = document.querySelector('.btn')
+const search = document.querySelector('.search')
+const input = document.querySelector('.input')
+
+searchButton.addEventListener('click', () =>{
+    search.classList.toggle('active')
+    input.focus()
+})
